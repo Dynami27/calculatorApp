91224712a0c3d0e62d2dd0107b2f5f9823a6a268
Dynami27 <Neuron@khalids-macbook-pro.local>
2016-09-28 12:06:16 -0500

    Initial Commit

---
+++ calculatorApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  calculatorApp
+//
+//  Created by Khalid Mohamed on 9/28/16.
+//  Copyright © 2016 Khalid Mohamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ calculatorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  calculatorApp
+//
+//  Created by Khalid Mohamed on 9/28/16.
+//  Copyright © 2016 Khalid Mohamed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
